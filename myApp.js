c3e13804dc08f4d852045f3e39b5237dd91e8e1b
KJWesthoff <karl.johan.westhoff@gmail.com>
2022-04-21 20:54:24 -0700

    added public path

--- myApp.js
+++ myApp.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var app = express();
+var path = require('path')
+console.log("Hello World");
 
-console.log("Hello World")
+console.log(path.join(__dirname, "public"))
 
-
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get('/', (req,res) => {
-
     //res.send("Hello Express")
-    res.sendFile(__dirname+"/views/index.html")
-
-
-
+    res.sendFile(__dirname+"/views/index.html");
 })
 
 
