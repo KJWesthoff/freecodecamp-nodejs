1065c192045e12813b864a2e6269055b7175a9c0
KJWesthoff <karl.johan.westhoff@gmail.com>
2022-04-21 23:19:15 -0700

    now chained a middelware in an endpoint

--- myApp.js
+++ myApp.js
@@ -34,7 +34,12 @@ app.get('/json', (req,res) => {
 })
 
 
-
+app.get("/now", (req,res,next)=> {
+    req.time = new Date().toString()
+    next()
+}, (req,res)=> {
+    res.json({time:req.time})
+})
 
 
 
